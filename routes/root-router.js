8ab6fb594f84792e212b8b5ce9cb6f0ae1e6dafe
“avganka” <karambasterg@gmail.com>
2022-06-22 22:35:27 +0300

    Обновлен основной роутер WIP

--- routes/root-router.js
+++ routes/root-router.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import PostRouter from './post-router.js';
+// import PostRouter from './post-router.js';
 import QuestionRouter from './question-router.js';
+import PackRouter from './pack-router.js';
+import CategoryRouter from './category-router.js';
 
 const router = new Router();
 
-router.use('/posts', PostRouter);
+// router.use('/posts', PostRouter);
 router.use('/questions', QuestionRouter);
+router.use('/packs', PackRouter);
+router.use('/categories', CategoryRouter);
 
 export default router;
